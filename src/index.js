2d5b86e667e8f5e920c68df5c6951c4559517595
LeticiaGarciah <128478702+LeticiaGarciah@users.noreply.github.com>
2024-03-19 13:40:08 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import "./reset.scss";
 
-import Routes from "./routes";
+import { Routes } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
