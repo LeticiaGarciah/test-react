8c3f61474beba482d42bf916eb0495f3e5312782
LeticiaGarciah <128478702+LeticiaGarciah@users.noreply.github.com>
2024-03-19 13:39:21 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,25 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import "./reset.scss";
 
-<<<<<<< Updated upstream
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import Login from "./pages/login";
-import Dashboard from "./pages/dash";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
-  },
-]);
-=======
-import { Routes } from "./routes";
->>>>>>> Stashed changes
+import Routes from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
